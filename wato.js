b745902c6eb6efaffb357ea71c3090bb1689a0e1
wotowato <watowoto@gmail.com>
2021-03-21 20:34:13 +0300

    assignment

---
+++ wato.js
@@ -0,0 +1,23 @@
+function getGrade(){
+    marks=81;
+
+if (marks>=80){
+    console.log("A");
+}
+else if(mark>=70){
+    console.log("B");
+}
+else if(mark>=60){
+    console.log("C");
+}
+else if(mark){
+    console.log("D");
+}
+else if(mark>=0){
+    console.log("E");
+}
+}
+getGrade()
+
+
+
